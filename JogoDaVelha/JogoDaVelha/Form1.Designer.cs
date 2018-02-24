734ed92c58bf49bd30b72a94296fc5c134c3a02d
Paulo Cordeiro <paulojcordeiro@hotmail.com>
2018-02-23 22:13:44 -0300

    Add files via upload

---
+++ JogoDaVelha/JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(11, 69);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(100, 100);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = true;
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(117, 69);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(100, 100);
+            this.b12.TabIndex = 1;
+            this.b12.UseVisualStyleBackColor = true;
+            this.b12.Click += new System.EventHandler(this.b12_Click);
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.Location = new System.Drawing.Point(225, 69);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(100, 100);
+            this.b13.TabIndex = 2;
+            this.b13.UseVisualStyleBackColor = true;
+            // 
+            // b23
+            // 
+            this.b23.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.Location = new System.Drawing.Point(225, 175);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(100, 100);
+            this.b23.TabIndex = 5;
+            this.b23.UseVisualStyleBackColor = true;
+            this.b23.Click += new System.EventHandler(this.b23_Click);
+            // 
+            // b22
+            // 
+            this.b22.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(117, 175);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(100, 100);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = true;
+            this.b22.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // b21
+            // 
+            this.b21.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(11, 175);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(100, 100);
+            this.b21.TabIndex = 3;
+            this.b21.UseVisualStyleBackColor = true;
+            // 
+            // b33
+            // 
+            this.b33.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.Location = new System.Drawing.Point(225, 281);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(100, 100);
+            this.b33.TabIndex = 8;
+            this.b33.UseVisualStyleBackColor = true;
+            // 
+            // b32
+            // 
+            this.b32.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.Location = new System.Drawing.Point(117, 281);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(100, 100);
+            this.b32.TabIndex = 7;
+            this.b32.UseVisualStyleBackColor = true;
+            // 
+            // b31
+            // 
+            this.b31.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.Location = new System.Drawing.Point(11, 281);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(100, 100);
+            this.b31.TabIndex = 6;
+            this.b31.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "x, é a sua vez";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 399);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JogoDaVelha/JogoDaVelha/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        bool xis = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            b11.Click += new EventHandler(BClick);
+            b12.Click += new EventHandler(BClick);
+            b13.Click += new EventHandler(BClick);
+            b21.Click += new EventHandler(BClick);
+            b22.Click += new EventHandler(BClick);
+            b23.Click += new EventHandler(BClick);
+            b31.Click += new EventHandler(BClick);
+            b32.Click += new EventHandler(BClick);
+            b33.Click += new EventHandler(BClick);
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.TabStop = false;
+                }
+            }
+        }
+
+        private void BClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.xis ? "X" : "0";
+            ((Button)sender).Enabled = false;
+
+            VerificarGanhador();
+
+            xis = !xis;
+
+            label1.Text = string.Format("{0}, é a sua vez", this.xis ? "X" : "0");
+        }
+
+        private void VerificarGanhador()
+        {
+            if (
+                b11.Text != String.Empty && b11.Text == b12.Text && b12.Text == b13.Text || //linha 1
+                b21.Text != String.Empty && b21.Text == b22.Text && b22.Text == b23.Text || //linha 2
+                b31.Text != String.Empty && b31.Text == b32.Text && b32.Text == b33.Text || //linha 3
+
+                b11.Text != String.Empty && b11.Text == b21.Text && b21.Text == b31.Text || //coluna 1
+                b12.Text != String.Empty && b12.Text == b22.Text && b22.Text == b32.Text || //coluna 2
+                b13.Text != String.Empty && b13.Text == b23.Text && b23.Text == b33.Text || //coluna 3
+
+                b11.Text != String.Empty && b11.Text == b22.Text && b22.Text == b33.Text || //diagonal \
+                b13.Text != String.Empty && b13.Text == b22.Text && b22.Text == b31.Text //diagonal /
+                )
+            {
+                MessageBox.Show(String.Format("O ganhador é o {0}", xis ? "X" : "0"), "Temos um vitorioso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reiniciar();
+            }
+            else
+            {
+                VerificarEmpate();
+            }
+        }
+
+        private void VerificarEmpate()
+        {
+            bool todosDesabilitados = true;
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button && item.Enabled)
+                {
+                    todosDesabilitados = false;
+                    break;
+                }                
+            }
+
+            if (todosDesabilitados)
+            {
+                MessageBox.Show(String.Format("Deu empate"), "Ops!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Reiniciar();
+            }
+        }
+
+        private void Reiniciar()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = true;
+                    item.Text = String.Empty;
+                }
+                {
+
+                }
+            }
+        }
+
+        private void b12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b23_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
